5f8c9b5fd6133a944182de8efc46d6fb36d69928
Bingus669 <thicccChungus1@gmail.com>
2025-12-27 23:07:53 -0600

    maybe fixed bug where skybox rotation was not persistent

--- src/skybox.rs
+++ src/skybox.rs
@@ -7,6 +7,7 @@ use crate::CloudCamera;
 #[derive(Component)]
 pub(crate) struct SkyboxPlane {
     pub orig_translation: Vec3,
+    pub orig_rotation: Quat,
 }
 
 pub(crate) struct SkyboxMaterials<M: Material> {
@@ -44,13 +45,14 @@ pub(crate) fn init_skybox_mesh<M: Material>(
     let mesh = meshes.add(Plane3d::new(Vec3::Y, Vec2::splat(box_size)));
 
     // negative x
+    let rotation_nx = Quat::from_rotation_z(-PI * 0.5) * Quat::from_rotation_y(PI * 0.5);
     commands.spawn((
         Mesh3d(mesh.clone()),
         standard_materials.nx,
-        Transform::from_translation(Vec3::new(-box_size, 0.0, 0.0))
-            .with_rotation(Quat::from_rotation_z(-PI * 0.5) * Quat::from_rotation_y(PI * 0.5)),
+        Transform::from_translation(Vec3::new(-box_size, 0.0, 0.0)).with_rotation(rotation_nx),
         SkyboxPlane {
             orig_translation: Vec3::new(-box_size, 0.0, 0.0),
+            orig_rotation: rotation_nx,
         },
     ));
 
@@ -61,50 +63,55 @@ pub(crate) fn init_skybox_mesh<M: Material>(
         Transform::from_translation(Vec3::new(0.0, -box_size, 0.0)),
         SkyboxPlane {
             orig_translation: Vec3::new(0.0, -box_size, 0.0),
+            orig_rotation: Quat::IDENTITY,
         },
     ));
 
     // negative z
+    let rotation_nz = Quat::from_rotation_x(PI * 0.5);
     commands.spawn((
         Mesh3d(mesh.clone()),
         standard_materials.nz,
-        Transform::from_translation(Vec3::new(0.0, 0.0, -box_size))
-            .with_rotation(Quat::from_rotation_x(PI * 0.5)),
+        Transform::from_translation(Vec3::new(0.0, 0.0, -box_size)).with_rotation(rotation_nz),
         SkyboxPlane {
             orig_translation: Vec3::new(0.0, 0.0, -box_size),
+            orig_rotation: rotation_nz,
         },
     ));
 
     // positive x
+    let rotation_px = Quat::from_rotation_z(PI * 0.5) * Quat::from_rotation_y(-PI * 0.5);
     commands.spawn((
         Mesh3d(mesh.clone()),
         standard_materials.px,
-        Transform::from_translation(Vec3::new(box_size, 0.0, 0.0))
-            .with_rotation(Quat::from_rotation_z(PI * 0.5) * Quat::from_rotation_y(-PI * 0.5)),
+        Transform::from_translation(Vec3::new(box_size, 0.0, 0.0)).with_rotation(rotation_px),
         SkyboxPlane {
             orig_translation: Vec3::new(box_size, 0.0, 0.0),
+            orig_rotation: rotation_px,
         },
     ));
 
     // positive y
+    let rotation_py = Quat::from_rotation_z(PI) * Quat::from_rotation_y(PI);
     commands.spawn((
         Mesh3d(mesh.clone()),
         standard_materials.py,
-        Transform::from_translation(Vec3::new(0.0, box_size, 0.0))
-            .with_rotation(Quat::from_rotation_z(PI) * Quat::from_rotation_y(PI)),
+        Transform::from_translation(Vec3::new(0.0, box_size, 0.0)).with_rotation(rotation_py),
         SkyboxPlane {
             orig_translation: Vec3::new(0.0, box_size, 0.0),
+            orig_rotation: rotation_py,
         },
     ));
 
     // positive z
+    let rotation_pz = Quat::from_rotation_x(-PI * 0.5) * Quat::from_rotation_y(PI);
     commands.spawn((
         Mesh3d(mesh.clone()),
         standard_materials.pz,
-        Transform::from_translation(Vec3::new(0.0, 0.0, box_size))
-            .with_rotation(Quat::from_rotation_x(-PI * 0.5) * Quat::from_rotation_y(PI)),
+        Transform::from_translation(Vec3::new(0.0, 0.0, box_size)).with_rotation(rotation_pz),
         SkyboxPlane {
             orig_translation: Vec3::new(0.0, 0.0, box_size),
+            orig_rotation: rotation_pz,
         },
     ));
 }
@@ -121,7 +128,7 @@ pub(crate) fn setup_daylight(mut commands: Commands) {
 
 pub(crate) fn update_skybox_transform(
     camera: Single<
-        (&Transform, &Camera, &Projection),
+        (&GlobalTransform, &Camera, &Projection),
         (Without<SkyboxPlane>, With<CloudCamera>),
     >,
     mut skybox: Query<(&mut Transform, &SkyboxPlane)>,
@@ -136,6 +143,7 @@ pub(crate) fn update_skybox_transform(
 
     for (mut transform, plane) in skybox.iter_mut() {
         transform.scale = Vec3::splat(scale);
-        transform.translation = camera.0.translation + plane.orig_translation * scale;
+        transform.translation = camera.0.translation() + plane.orig_translation * scale;
+        transform.rotation = plane.orig_rotation;
     }
 }
